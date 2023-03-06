9e44f03e0be9ee29b483c29a1637a696028b5be1
piner <qjksxy@163.com>
2023-03-06 22:04:59 +0800

    feat: :sparkles: 完善 ui

--- ui.py
+++ ui.py
@@ -32,24 +32,27 @@ step_records2 = []
 # 记录得分
 score_records1 = []
 score_records2 = []
+board = []
 
 # 定义画布
 canvas = tk.Canvas(top, height=mapsize * pixsize, width=mapsize * pixsize, bg = "gray")
 
 def canvas_init():
-    global canvas, row, row_bak, white_board, stepBoard, black_board
+    global canvas
     canvas.pack(pady=10)
     for i in range(mapsize):
         canvas.create_line(i * pixsize, 0, i * pixsize, mapsize * pixsize, fill='black')
         canvas.create_line(0, i * pixsize, mapsize * pixsize, i * pixsize, fill='black')
-    for i in range(mapsize):
-        for j in range(mapsize):
-            row.append(blankcode)
-            row_bak.append(blankcode)
-        white_board.append(row_bak)
-        stepBoard.append(row)
-    black_board = copy.deepcopy(white_board)
+    for i in range(mapsize * mapsize):
+        board.append(blankcode)
     restart()
+
+def get_board(x, y):
+    return board[y * mapsize + x]
+
+def set_board(x, y, value):
+    global board
+    board[y * mapsize + x] = value
 
 def restart():
     global is_game_over
@@ -61,16 +64,9 @@ def restart():
     turn_counter = 0
     is_game_over = False
     is_turn_black = True
-    map_records1.clear()
-    map_records2.clear()
-    step_records1.clear()
-    step_records2.clear()
-    score_records1.clear()
-    score_records2.clear()
-    for i in range(mapsize):
-        for j in range(mapsize):
-            white_board[j][i] = blankcode
-            black_board[j][i] = blankcode
+    for i in range(mapsize * mapsize):
+        board[i] = blankcode
+    # 下第一子
     chess(mapsize//2, mapsize//2, 0)
     is_turn_black = False
     turn_counter = 0
@@ -80,32 +76,21 @@ def touch_canvas(event):
     # 获取 x y 坐标
     x = event.x // pixsize
     y = event.y // pixsize
-    if x >= mapsize or y >= mapsize:
-        print('touch_canvas: return: x >= mapsize or y >= mapsize')
-        return
-    if white_board[y][x] != blankcode:
-        print('touch_canvas: return: white_board[y][x] != blankcode')
+    if x >= mapsize or y >= mapsize or get_board(x, y) != blankcode:
         return
     chess(x, y, 0)
 
 def chess(x, y, score):
     global is_turn_black
     global turn_counter
-    if is_game_over or white_board[y][x] != blankcode:
+    if is_game_over or get_board(x, y) != blankcode:
         return -1
-    step = copy.deepcopy(stepBoard)
-    step[y][x] = 1
-    map_records1.append(copy.deepcopy(black_board))
-    step_records1.append(step)
-    score_records1.append(score)
     if is_turn_black:
         fill_color = 'black'
-        white_board[y][x] = whitecode  # 1白 -1黑
-        black_board[y][x] = blackcode
+        set_board(x, y, blackcode)
     else:
         fill_color = 'white'
-        white_board[y][x] = blackcode  # 1白 -1黑
-        black_board[y][x] = whitecode
+        set_board(x, y, whitecode)
     child = canvas.create_oval(x * pixsize,
                                y * pixsize,
                                x * pixsize + pixsize,
@@ -136,7 +121,7 @@ btnUp.pack()
 
 # 添加按钮  -- 开始游戏
 def start_game_btn():
-    # restart()
+    restart()
     pass
 startGameBtn = tk.Button(top, text="开始游戏/重新开始", command=start_game_btn)
 startGameBtn.pack()
